a383a922e69ea95761b5c3e16b3c71cc0348681a
Yeriza\Alex <allenitsmail@gmail.com>
2018-12-19 22:44:26 +0100

    v3.17 -made changes to the scripts, they are simpler and more concurent (i.e sends the "at the same time")

--- Distributed Systems/scripts/test_6nodes.sh
+++ Distributed Systems/scripts/test_6nodes.sh
@@ -1,5 +1,25 @@
 #! /bin/bash
 echo "test for labs"
-for i in `seq 0 30`; do #5 posts from each node
-	curl -d 'entry=t'${i} -X 'POST' 'http://10.1.0.'$(((i%6)+1))'/board' &
+for i in `seq 0 19`; do #20 posts from node 1
+	curl -d 'entry=t'${i} -X 'POST' 'http://10.1.0.1/board' &
+done
+
+for i in `seq 20 39`; do #20 posts from node 2
+	curl -d 'entry=t'${i} -X 'POST' 'http://10.1.0.2/board' &
+done
+
+for i in `seq 40 59`; do #20 posts from node 3
+	curl -d 'entry=t'${i} -X 'POST' 'http://10.1.0.3/board' &
+done
+
+for i in `seq 60 79`; do #20 posts from node 4
+	curl -d 'entry=t'${i} -X 'POST' 'http://10.1.0.4/board' &
+done
+
+for i in `seq 80 99`; do #20 posts from node 5
+	curl -d 'entry=t'${i} -X 'POST' 'http://10.1.0.5/board' &
+done
+
+for i in `seq 100 119`; do #20 posts from node 6
+	curl -d 'entry=t'${i} -X 'POST' 'http://10.1.0.6/board' &
 done
--- Distributed Systems/scripts/test_8nodes.sh
+++ Distributed Systems/scripts/test_8nodes.sh
@@ -1,5 +1,34 @@
 #! /bin/bash
 echo "test for labs"
-for i in `seq 0 40`; do #5 posts from each node
-	curl -d 'entry=t'${i} -X 'POST' 'http://10.1.0.'$(((i%8)+1))'/board' &
+for i in `seq 0 19`; do #20 posts from node 1
+	curl -d 'entry=t'${i} -X 'POST' 'http://10.1.0.1/board' &
 done
+
+for i in `seq 20 39`; do #20 posts from node 2
+	curl -d 'entry=t'${i} -X 'POST' 'http://10.1.0.2/board' &
+done
+
+for i in `seq 40 59`; do #20 posts from node 3
+	curl -d 'entry=t'${i} -X 'POST' 'http://10.1.0.3/board' &
+done
+
+for i in `seq 60 79`; do #20 posts from node 4
+	curl -d 'entry=t'${i} -X 'POST' 'http://10.1.0.4/board' &
+done
+
+for i in `seq 80 99`; do #20 posts from node 5
+	curl -d 'entry=t'${i} -X 'POST' 'http://10.1.0.5/board' &
+done
+
+for i in `seq 100 119`; do #20 posts from node 6
+	curl -d 'entry=t'${i} -X 'POST' 'http://10.1.0.6/board' &
+done
+
+for i in `seq 120 139`; do #20 posts from node 7
+	curl -d 'entry=t'${i} -X 'POST' 'http://10.1.0.7/board' &
+done
+
+for i in `seq 140 159`; do #20 posts from node 8
+	curl -d 'entry=t'${i} -X 'POST' 'http://10.1.0.8/board' &
+done
+
